1e72d3d847d98ad2da4ea583d208d219d6239417
cbin <2309970475@qq.com>
2018-04-23 06:25:47 +0000

    Finish layout and router

---
+++ resources/views/layouts/_herder.blade.php
@@ -0,0 +1,13 @@
+<header class="navbar navbar-fixed-top navbar-inverse">
+    <div class="content">
+        <div class="col-md-offset-1 col-md-10">
+            <a herf="{{ route('home') }}" id="logo">Sample App</a>
+            <nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{ route('help') }}">帮助</a></li>
+                    <li><a href="#">登入</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,17 @@
 <html>
     <head>
         <title>@yield('title','Sample') - Laravel 新手入门教程</title>
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
-        @yield('content')
+        @include('layouts._herder')
+
+        <div class="content">
+            <div class="col-md-8 col-md-offset-2">
+                @yield('content')
+                @include('layouts._footer')
+            </div>
+        </div>
+
     </body>
 </html>
